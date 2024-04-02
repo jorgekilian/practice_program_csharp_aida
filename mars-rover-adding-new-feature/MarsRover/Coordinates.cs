8ffd443b9f7a983c7aee718db3754523bfe5240a
trikitrok <trikitrok@gmail.com>
2024-04-02 19:46:56 +0200

    making record immutable

--- mars-rover-adding-new-feature/MarsRover/Coordinates.cs
+++ mars-rover-adding-new-feature/MarsRover/Coordinates.cs
@@ -1,14 +1,23 @@
 namespace MarsRover;
 
-internal record Coordinates(int X, int Y)
+internal record Coordinates
 {
+    private readonly int _x;
+    private readonly int _y;
+
+    public Coordinates(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
+
     public Coordinates MoveAlongYAxis(int displacement)
     {
-        return new Coordinates(X, Y + displacement);
+        return new Coordinates(_x, _y + displacement);
     }
 
     public Coordinates MoveAlongXAxis(int displacement)
     {
-        return new Coordinates(X + displacement, Y);
+        return new Coordinates(_x + displacement, _y);
     }
 }
