a397c147eb1a7db088904e97071007608c8e2c38
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-06-11 09:30:00 +0100

    greet with "buenas noches" without conditions

---
+++ HelloKata/HelloKata/HelloKataTest/HelloKataTest.cs
@@ -0,0 +1,48 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace HelloKata.Test {
+
+
+    public class HelloKataTest {
+
+        // Lista de Tests
+        // Devolver "buenas noches"
+        // Devolver "buenos días" si la hora es entre las 6:00 AM y las 12:00AM
+        // Devolver "buenas tardes" si la hora es entre las 12:00 y las 8:00PM
+
+        // Colaboradores
+        // Notifier
+        // ¿Uso horario?
+        private Notifier _notifier;
+
+        [Test]
+        public void greet_with_good_night() {
+
+            _notifier = Substitute.For<Notifier>();
+            var helloKata = new HelloKata(_notifier);
+
+            helloKata.Hello();
+
+            _notifier.Received(1).Notify("Buenas noches");
+
+        }
+
+    }
+
+    public interface Notifier {
+        void Notify(string greetMessage);
+    }
+
+    public class HelloKata {
+        private readonly Notifier notifier;
+
+        public HelloKata(Notifier notifier) {
+            this.notifier = notifier;
+        }
+
+        public void Hello() {
+            notifier.Notify("Buenas noches");
+        }
+    }
+}
