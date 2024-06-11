d5c2251e24f76d502e6bbc34444696b2f5b43cd7
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-06-11 12:58:56 +0100

    process one  order of type B

--- stock-broker-service/StockBroker.Tests/StockBrokerServiceTest.cs
+++ stock-broker-service/StockBroker.Tests/StockBrokerServiceTest.cs
@@ -1,13 +1,68 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace StockBroker.Tests
 {
     public class StockBrokerServiceTest
     {
+        //Tests
+        // Una orden de tipo B
+        // Una orden de tipo S
+        // Dos ordenes de tipo B
+        // Dos ordenes de tipo S
+        // Dos ordenes, una de tipo B y otra de tipo S
+        // Tres ordenes, dos de tipo B y una de tipo S
+        // Tres ordenes, una de tipo B y dos de tipo S
+        // Cuatro ordenes, dos de tipo B y dos de tipo S
+        // Una orden que fallará
+        // Dos ordenes y una fallará
+        // Tres ordenes y dos fallaran
+
+        //Colaboradores
+        // Notifier
+        // Servicio Broker Online
+        // DatetimeProvider
+
+        DateTimeProvider _dateTimeProvider;
+        StockBrokerService _stockBrokerService;
+        Notifier _notifier;
+
+
         [Test]
-        public void Canary_Test()
+        public void process_one_type_order_B()
         {
-            Assert.That(true, Is.True);
+            _notifier = Substitute.For<Notifier>();
+            _stockBrokerService = Substitute.For<StockBrokerService>();
+            _dateTimeProvider = Substitute.For<DateTimeProvider>();
+            var stockBrokerClient = new StockBrokerClient(_notifier, _stockBrokerService, _dateTimeProvider);
+            
+            stockBrokerClient.PlaceOrders("GOOG 300 829.08 B");
+
+            _notifier.Received(1).Notify("12/20/2023 1:45 AM Buy: € 248724.00, Sell: € 0.00");
         }
     }
+
+    public class StockBrokerClient {
+        private readonly Notifier notifier;
+        private readonly StockBrokerService stockBrokerService;
+        private readonly DateTimeProvider dateTimeProvider;
+
+        public StockBrokerClient(Notifier notifier, StockBrokerService stockBrokerService, DateTimeProvider dateTimeProvider) {
+            this.notifier = notifier;
+            this.stockBrokerService = stockBrokerService;
+            this.dateTimeProvider = dateTimeProvider;
+        }
+
+        public void PlaceOrders(string ordersSequence) {
+            this.notifier.Notify("12/20/2023 1:45 AM Buy: € 248724.00, Sell: € 0.00");
+        }
+    }
+
+    public interface Notifier {
+        void Notify(string message);
+    }
+
+    public interface StockBrokerService { }
+
+    public interface DateTimeProvider { }
 }
