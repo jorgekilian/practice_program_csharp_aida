5a476d14e0c54652ad3d0302b3923173912be100
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-06-18 12:30:04 +0100

    delete unnecessary test

--- checkout-with-side-effects/Checkout.Tests/CheckoutTest.cs
+++ checkout-with-side-effects/Checkout.Tests/CheckoutTest.cs
@@ -2,21 +2,36 @@ using NUnit.Framework;
 
 namespace Checkout.Tests;
 
-public class CheckoutTest
-{
+public class CheckoutTest {
     [Test]
-    public void checkout_create_and_store_receipt()
-    {
+    public void checkout_create_and_store_receipt() {
         var checkout = new ForTestingCheckout();
 
         var receipt = checkout.CreateReceipt(new Money(12));
         var expectedReceived = new Receipt(new Money(12m), new Money(2.4m), new Money(14.4m));
 
         Assert.That(checkout.storedReceipts.First(), Is.EqualTo(expectedReceived));
+        Assert.That(checkout.storedReceipts.Count, Is.EqualTo(1));
         Assert.That(receipt, Is.EqualTo(expectedReceived));
 
     }
 
+    [Test]
+    public void checkout_create_and_store_two_receipts() {
+        var checkout = new ForTestingCheckout();
+        var expectedReceived = new List<Receipt>();
+
+
+        var receipt = checkout.CreateReceipt(new Money(12));
+        var otherReceipt = checkout.CreateReceipt(new Money(100));
+        expectedReceived.Add(new Receipt(new Money(12m), new Money(2.4m), new Money(14.4m)));
+        expectedReceived.Add(new Receipt(new Money(100m), new Money(20m), new Money(120m)));
+
+        Assert.That(checkout.storedReceipts, Is.EquivalentTo(expectedReceived));
+        Assert.That(receipt, Is.EqualTo(expectedReceived[0]));
+        Assert.That(otherReceipt, Is.EqualTo(expectedReceived[1]));
+        Assert.That(checkout.storedReceipts.Count, Is.EqualTo(2));
+    }
 
     public class ForTestingCheckout : Checkout {
         public List<Receipt> storedReceipts;
