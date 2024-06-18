bd051f1e19b1b3ab0f0a0e394ce09ec513fc1e33
trikitrok <trikitrok@gmail.com>
2024-06-18 12:21:42 +0200

    new kata

---
+++ checkout-with-side-effects/Checkout/Money.cs
@@ -0,0 +1,41 @@
+﻿namespace Checkout;
+
+public record Money
+{
+    private readonly decimal _value;
+
+    public Money(int value)
+    {
+        _value = value;
+    }
+
+    public Money(decimal value)
+    {
+        _value = value;
+    }
+
+    public Money Add(Money other)
+    {
+        return new Money(_value + other._value);
+    }
+
+    public Money Percentage(int p)
+    {
+        return new Money(_value * p / 100);
+    }
+
+    public string Format()
+    {
+        return $"{_value:0.00}";
+    }
+
+    internal decimal AsDecimal()
+    {
+        return _value;
+    }
+
+    public override string ToString()
+    {
+        return "Money { " + _value + " }";
+    }
+}
